c0b1fa47c8d00d5839ec1aefc50766778aeb6f64
Sma1lboy <chen.10724@buckeyemail.osu.edu>
2024-02-05 12:35:27 -0600

    feat: implement get screenshot of current located when we input a local screenshot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import  webdriver
 from selenium.webdriver.common.by import By
+
 import requests
 import keyboard as kb
 import time
@@ -12,14 +13,44 @@ import atexit
 
 path = Path(os.path.expanduser("~")) / 'Documents' / 'Escape from Tarkov' / 'Screenshots'
 
-def getFile():
+def get_position_file():
     files = os.listdir(path)
     if len(files) == 0:
         return;
     res = files[0]
-    file = path + files[0]
+    file = path / files[0]
     os.remove(file)
     return res
 
+def delete_useless_element(driver):
+    leftPane = driver.find_element(By.XPATH, "/html/body/div/div/div/div[2]/div/div/div[2]")
+    header = driver.find_element    (By.XPATH,"/html/body/div/div/div/header")
+    rightPane = driver.find_element(By.XPATH,"/html/body/div/div/div/div[2]/div/div/div[3]")
+    superHeader = driver.find_element(By.XPATH,"/html/body/div/div/div/div[1]")
+    footer = driver.find_element(By.XPATH, "/html/body/div/div/div/div[3]")
+    topPane = driver.find_element(By.XPATH, "//*[@id=\"__nuxt\"]/div/div/div[2]/div/div/div[1]")
+    elements = [header, leftPane, rightPane, superHeader, footer, topPane]
+
+    for element in elements:
+        if element:
+            driver.execute_script("arguments[0].remove();", element)
+
+    return
 if __name__ == "__main__":
-    print("init proj")
+    driver = webdriver.Chrome()
+    driver.get("https://tarkov-market.com/maps/factory")
+    
+    # get map ready
+    button = driver.find_element(By.XPATH, "//*[@id=\"__nuxt\"]/div/div/div[2]/div/div/div[1]/div/button")
+    time.sleep(1)
+    button.click()
+    input = driver.find_element(By.XPATH, "//*[@id=\"__nuxt\"]/div/div/div[2]/div/div/div[1]/div/input")
+    # input.send_keys(get_position_file())
+
+    delete_useless_element(driver)
+    # screenshot map
+    image = driver.get_screenshot_as_png()
+    with open("screenshot.png","wb") as file :
+        file.write(image)
+    print()
+    driver.quit()
