d35b593f68fd179ea85e7314dc26405f3cd33827
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-02-06 11:18:23 -0600

    feat: optimaztion network with webdriver, it will not cost too much time for open webdriver and close web driver again. much faster

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from selenium import  webdriver
 from selenium.webdriver.common.by import By
 
 import threading
-import requests
 import keyboard as kb
 import time
 import os
@@ -10,9 +9,8 @@ import io
 from pathlib import Path
 from PIL import Image, ImageTk
 import json
-import traceback
-import atexit
 import tkinter as tk
+import sv_ttk
 from tkinter import ttk;
 
 path = Path(os.path.expanduser("~")) / 'Documents' / 'Escape from Tarkov' / 'Screenshots'
@@ -42,21 +40,24 @@ def delete_useless_element(driver):
     return
 
 def fetchImage(map:str):
+    global driver
     try: 
-        options = webdriver.ChromeOptions();
-        options.add_argument("--headless")
-        driver = webdriver.Chrome(options)
-        driver = webdriver.Chrome()
-        driver.get("https://tarkov-market.com/maps/" + map)
-        # get map ready
-        button = driver.find_element(By.XPATH, "//*[@id=\"__nuxt\"]/div/div/div[2]/div/div/div[1]/div/button")
-        time.sleep(0.5)
-        button.click()
+        if "driver" not in globals():
+            options = webdriver.ChromeOptions();
+            options.add_argument("--headless")
+            driver = webdriver.Chrome(options)
+        if "driver" in globals() and driver.current_url != ("https://tarkov-market.com/maps/" + map):
+            driver.get("https://tarkov-market.com/maps/" + map)
+            # get map ready
+            button = driver.find_element(By.XPATH, "//*[@id=\"__nuxt\"]/div/div/div[2]/div/div/div[1]/div/button")
+            time.sleep(0.5)
+            button.click()
         input = driver.find_element(By.XPATH, "//*[@id=\"__nuxt\"]/div/div/div[2]/div/div/div[1]/div/input")
+        # Todo dev version, uncomment this later
         # input.send_keys(get_position_file())
         delete_useless_element(driver)
         res = driver.get_screenshot_as_png()
-    finally:
+    except:
         driver.quit()
     return res;
 
@@ -111,52 +112,56 @@ def create_livemap_window():
     imageLabel.pack()
     
 if __name__ == "__main__":
-    global isAutoRefresh, screenshotKey, currMap
-    get_config()
-    window = tk.Tk()
-    window.title("EFTarkov Livemap")
+    global isAutoRefresh, screenshotKey, currMap, driver
+    try:
+        get_config()
+        window = tk.Tk()
+        window.title("EFTarkov Livemap")
 
-    introductionText = tk.Label(text="")
+        introductionText = tk.Label(text="")
 
-    currKeyFrame = tk.Frame(master=window).grid(row=0)
-    introLabel = tk.Label(master=currKeyFrame, text="Welcome to Tarkov Live Map, the first version of this open source software.\nUse F5 to stop the program, F6 to start livemap,\n or you can use the custom screenshot button to update the map location when you want.", justify=tk.LEFT).grid(sticky="w",row=0, column=0)
-    currentKeyLabel = tk.Label(master=currKeyFrame, text="the current key you are using: ").grid(sticky="w", row=1, column=0)
-    entry = tk.Entry()
-    entry.insert(0, screenshotKey)
-    entry.grid(row=1,column=1)
+        currKeyFrame = tk.Frame(master=window).grid(row=0)
+        introLabel = tk.Label(master=currKeyFrame, text="Welcome to Tarkov Live Map, the first version of this open source software.\nUse F5 to stop the program, F6 to start livemap,\n or you can use the custom screenshot button to update the map location when you want.", justify=tk.LEFT).grid(sticky="w",row=0, column=0)
+        currentKeyLabel = tk.Label(master=currKeyFrame, text="the current key you are using: ").grid(sticky="w", row=1, column=0)
+        entry = tk.Entry()
+        entry.insert(0, screenshotKey)
+        entry.grid(row=1,column=1)
 
-    currentKeyLabel = tk.Label(master=currKeyFrame, text="current map choicing: ").grid(sticky="w", row=2, column=0)
-    mapCombobox = ttk.Combobox(master=currKeyFrame, state="readonly")
-    map_opts_dict = {
-    'Factory': 'factory',
-    'Ground Zero': 'ground-zero',
-    'Customs': 'customs',
-    'Interchange': 'interchange',
-    'Woods': 'woods',
-    'Shoreline': 'shoreline',
-    'Reserve': 'reserve',
-    'Lighthouse': 'lighthouse',
-    'Street of Tarkov': 'streets',
-    'The Lab': 'lab' } 
-    map_opts = [key for key, value in map_opts_dict.items()] 
-    mapCombobox["values"] = map_opts
-    mapCombobox.current(0) #setup default map
-    mapCombobox.grid(row=2,column=1)
-    def map_selected_event(e):
-        global currMap
-        currMap = map_opts_dict[mapCombobox.get()]
-        print("current map is ", currMap)
-    mapCombobox.bind("<<ComboboxSelected>>", map_selected_event)
-    currentStatusLabel = tk.Label(text="current status: ").grid(sticky="w",row=3, column=0)
-    global statusLabel
-    statusLabel = tk.Label(text="Not start")
-    statusLabel.grid(row=3, column=1)
-    kb_thread = threading.Thread(target=keyboard_listener_thread)
-    kb_thread.daemon = True
-    kb_thread.start()
+        currentKeyLabel = tk.Label(master=currKeyFrame, text="current map choice: ").grid(sticky="w", row=2, column=0)
+        mapCombobox = ttk.Combobox(master=currKeyFrame, state="readonly")
+        map_opts_dict = {
+        'Factory': 'factory',
+        'Ground Zero': 'ground-zero',
+        'Customs': 'customs',
+        'Interchange': 'interchange',
+        'Woods': 'woods',
+        'Shoreline': 'shoreline',
+        'Reserve': 'reserve',
+        'Lighthouse': 'lighthouse',
+        'Street of Tarkov': 'streets',
+        'The Lab': 'lab' } 
+        map_opts = [key for key, value in map_opts_dict.items()] 
+        mapCombobox["values"] = map_opts
+        mapCombobox.current(0) #setup default map
+        mapCombobox.grid(row=2,column=1)
+        def map_selected_event(e):
+            global currMap
+            currMap = map_opts_dict[mapCombobox.get()]
+            print("current map is ", currMap)
+        mapCombobox.bind("<<ComboboxSelected>>", map_selected_event)
+        currentStatusLabel = tk.Label(text="current status: ").grid(sticky="w",row=3, column=0)
+        global statusLabel
+        statusLabel = tk.Label(text="Not start")
+        statusLabel.grid(row=3, column=1)
+        kb_thread = threading.Thread(target=keyboard_listener_thread)
+        kb_thread.daemon = True
+        kb_thread.start()
 
-    create_livemap_window()
+        create_livemap_window()
 
+        sv_ttk.set_theme("dark")
+        window.mainloop()
+    finally:
+        if "driver" in globals():
+            driver.close()
 
-
-    window.mainloop()
